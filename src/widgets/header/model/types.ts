daeae71c652cf9987278a003a735317083d04168
Константин Новоселов <80128673+knstntnovoslv@users.noreply.github.com>
2026-05-30 16:41:41 +0300

    Delete src directory

--- src/widgets/header/model/types.ts
+++
@@ -1,4 +0,0 @@
-export interface INavItem {
-  title: string
-  expand?: boolean
-}
--- src/widgets/offerBanner/model/const.ts
+++
@@ -1,7 +0,0 @@
-
-
-export const title = "8 Видео сессий"
-
-export const description = "Проработать небольшие проблемы и продвинуться в решении сложных запросов"
-
-export const price = "18 500 ₽ "
